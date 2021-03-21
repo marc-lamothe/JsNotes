37d0b4e2bf8b9878c2023b2da27a3480eb7a0afb
Marc Lamothe <66534112+marc-lamothe@users.noreply.github.com>
2021-03-21 15:20:51 -0400

    2 pillars

---
+++ the2Pillars/00-the2Pillars.js
@@ -0,0 +1,35 @@
+// *  The 2 Pillars
+
+//  These two 'powers' are quite unique, and not available in all languages. They are what
+//    makes JavaScript so powerful. Senior JavaScript Developers MUST know these inside and
+//    out.
+
+// ?  What are these 'special skills'?
+// *  Closures
+// *  Prototypal Inheritance
+
+// But before we dive into those topics, we should first investigate:
+// *  High Order Functions
+// *  Functions vs Objects
+// *  Scheme + Java:  ( The birth of JS )
+
+// Once we understand these topics and 'The 2 Pillars, we can move onto Object Oriented
+//  Programming, Composition vs Inheritance, Functional Programming,
+
+// ? Included within Object Oriented Programming (OOP):
+// *  this keyword
+// *  new keyword
+// *  Prototype
+// *  ES6 Classes
+// *  Java
+// *  Inheritance
+// *  Object.create()
+// *  Private vs Public
+
+// ? Included within Functional Programming (FP):
+// *  Curry
+// *  Partial Application
+// *  Pure Functions
+// *  Referential Transparency
+// *  Compose
+// *  Pipe
--- the2Pillars/01-functionsAreObjects.js
+++ the2Pillars/01-functionsAreObjects.js
@@ -1,3 +1,93 @@
 // *  Functions are Objects
 
+//  Let's start by talking about Functions...
+
+//               Object {}
+//               /     \
+//          Array[]    Function()
+
+// Functions in JavaScript, are also Objects. Let's explore that...
+
+// TODO  In Review: Functions()
+//    When we invoke a function we get two parameters automatically, the 'this' keyword and
+//      'arguments' keyword.
+//    Arguments is an 'array-like' object that has some weird behaviours for looping and
+//      iteration, so we want to avoid using it, preferring the Spread Operator. Additionally,
+//      because of the arguments object we can technically not have any parameters defined
+//      for a function, and when we call the function, if we add parameters to it, we can
+//      still grab them using the arguments keyword.
+//    When our functions are defined the compiler looks at our code, it lexcially figures out
+//      which variables are available to us in the variable environment, and it also adds
+//      our scope chains.
+// ?  We also have a few ways of evoking and creating functions:
+// *   The traditional way:
+function one() {
+  return 1;
+}
+
+one();
 //
+// *   As a Method: ( a function inside of an object )
+const obj = {
+  two() {
+    return 2;
+  },
+};
+
+obj.two();
+//  Because the OBJECT is calling two, 'this' is assigned to 'obj' instead of 'window'.
+//
+// *  Call and Apply
+function three() {
+  return 3;
+}
+
+three.call();
+// OR
+three();
+//
+// * Function Constructor
+const four = new Function('return 4');
+
+four();
+//
+//
+// Because all functions are objects in JS, we can do things like:
+function woohoo() {
+  console.log('woooohooooo!');
+}
+
+woohoo.yell = 'ahhhhh!!!!';
+// ? Behind the Scenes
+//    const specialObj = {
+//      yell: 'ahhhhh!!!!',
+//      name: 'woooohooooo!',
+//      (): console.log('woooohooooo!')
+//    }
+//
+// * To Review:
+//  _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+// |                                                                                      |
+// |  somefunc()                                                                          |
+// |                                                                                      |
+// |   ________                                                                           |
+// |  | Code() |                                                                          |
+// |   ````````                                                                           |
+// |   _________________                                         ____________             |
+// |  | Name (optional) |                                       | Properties |            |
+// |   `````````````````                                         ````````````             |
+// |                                                          /       |       \           |
+// |                                                 _________   __________   _________   |
+// |                                                | .call() | | .apply() | | bind.() |  |
+// |                                                 `````````   ``````````   `````````   |
+// |                                                                                      |
+//  ```````````````````````````````````````````````````````````````````````````````````````
+//
+woohoo.name; // woooohooooo!
+woohoo.yell; // ahhhhh!!!!'
+//
+// Functions are Objects, however Functions are considered 'special' objects that are callable.
+//  This is important because functions are just objects in JS, and that means we can pass
+//  them around like things that contain data. So besides just doing things for us, and
+//  performing actions in our code, we can also store them as data, move them around, and
+//  have some interesting applications...
